42e319cee4bf874575131a92d6ad2e6b449f2e65
huongnv <huowngnv@jamja.vn>
2019-12-06 15:11:09 +0700

    init

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from subprocess import call
+
+def convert_readme():
+    try:
+        call(["pandoc", "-f", "markdown_github", "-t",  "rst", "-o",  "README.txt", "readme.md"])
+    except OSError:
+        pass
+    return open('README.txt').read()
+
+setup(name='mongodbforms',
+    version='0.3.1',
+    description="An implementation of django forms using mongoengine.",
+    author='Jan Schrewe',
+    author_email='jan@schafproductions.com',
+    url='http://www.schafproductions.com/projects/django-mongodb-forms/',
+    packages=['mongodbforms',],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    license='New BSD License',
+    long_description=convert_readme(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools', 'django>=1.4', 'mongoengine>=0.8.3',],
+)
